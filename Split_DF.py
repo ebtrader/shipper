7ed64349267bf9cc1b4b11c143615e8f100bedaa
ebtrader <jsiddique@gmail.com>
2020-05-08 16:37:16 -0400

    Add files via upload

--- Split_DF.py
+++ Split_DF.py
@@ -1,11 +1,18 @@
 import pandas as pd
 
 df = pd.DataFrame({'Sales':[10,20,30,40,50], 'A':[3,4,7,6,1], 'Shipper':['Fedex', 'Fedex', 'UPS', 'UPS', 'Conway']})
-print (df)
+#print (df)
 
 s = 30
 
 #df1 = df[df['Sales'] >= s]
 df1 = df[df['Shipper'] == 'Fedex']
+df2 = df[df['Shipper'] == 'UPS']
+df3 = df[df['Shipper'] == 'Conway']
 
 print(df1)
+print ()
+print(df2)
+print ()
+print(df3)
+print ()
